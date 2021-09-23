0a111a6464aa8e98b70fb507e0aa7c4631f31368
Qurrotul Ainun Ni'matus Sholihah <59301024+qurrotulainun@users.noreply.github.com>
2021-09-23 17:52:12 +0700

    Add files via upload

---
+++ Praktikum 2.R
@@ -0,0 +1,19 @@
+A <- c (1,2,3,4,5,6,7,8,9,10)
+A
+B <- c (11,12,13,14,15,16,17,18,19,20)
+B
+C <- c (A*3)
+C
+D <- c (B*2)
+D
+C[1]
+D[2]
+D
+D[4] <- 2
+D
+Z <- c (C+D)
+Z
+X <- c (Z/5)
+X
+names(X) <- c ("Ikan", "Sapi", "Ayam", "Kucing", "Anjing", "Burung", "Harimau", "Kambing", "Hamster", "Tikus")
+X
